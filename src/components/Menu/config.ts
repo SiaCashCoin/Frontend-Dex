5e1863186a168b4a715e7bc90d49f297ec8b8da6
SiaCashCoin <contact@siacashcoin.com>
2021-12-02 18:09:01 -0500

    Frontend-Dex

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,114 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: 'https://siacashcoin.com',
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    initialOpenState: true,
+    items: [
+      {
+        label: 'Exchange',
+        href: '/swap',
+      },
+      {
+        label: 'Liquidity',
+        href: '/pool',
+      },
+    ],
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: 'https://siacashcoin.com/farms',
+  },
+  {
+    label: 'Pools',
+    icon: 'PoolIcon',
+    href: 'https://siacashcoin.com/syrup',
+  },
+  // {
+  //   label: 'Lottery',
+  //   icon: 'TicketIcon',
+  //   href: 'https://siacashcoin.com/lottery',
+  // },
+  // {
+  //   label: 'NFT',
+  //   icon: 'NftIcon',
+  //   href: 'https://siacashcoin.com/nft',
+  // },
+  // {
+  //   label: 'Teams & Profile',
+  //   icon: 'GroupsIcon',
+  //   calloutClass: 'rainbow',
+  //   items: [
+  //     {
+  //       label: 'Leaderboard',
+  //       href: 'https://siacashcoin.com/teams',
+  //     },
+  //     {
+  //       label: 'Task Center',
+  //       href: 'https://siacashcoin.com/profile/tasks',
+  //     },
+  //     {
+  //       label: 'Your Profile',
+  //       href: 'https://siacashcoin.com/profile',
+  //     },
+  //   ],
+  // },
+  // {
+  //   label: 'Info',
+  //   icon: 'InfoIcon',
+  //   items: [
+  //     {
+  //       label: 'Overview',
+  //       href: 'https://pancakeswap.info',
+  //     },
+  //     {
+  //       label: 'Tokens',
+  //       href: 'https://pancakeswap.info/tokens',
+  //     },
+  //     {
+  //       label: 'Pairs',
+  //       href: 'https://pancakeswap.info/pairs',
+  //     },
+  //     {
+  //       label: 'Accounts',
+  //       href: 'https://pancakeswap.info/accounts',
+  //     },
+  //   ],
+  // },
+  // {
+  //   label: 'IFO',
+  //   icon: 'IfoIcon',
+  //   href: 'https://siacashcoin.com/ifo',
+  // },
+  // {
+  //   label: 'More',
+  //   icon: 'MoreIcon',
+  //   items: [
+  //     {
+  //       label: 'Voting',
+  //       href: 'https://voting.siacashcoin.com',
+  //     },
+  //     {
+  //       label: 'Github',
+  //       href: 'https://github.com/pancakeswap',
+  //     },
+  //     {
+  //       label: 'Docs',
+  //       href: 'https://docs.siacashcoin.com',
+  //     },
+  //     {
+  //       label: 'Blog',
+  //       href: 'https://pancakeswap.medium.com',
+  //     },
+  //   ],
+  // },
+]
+
+export default config
---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,43 @@
+import React, { useContext } from 'react'
+import { Menu as UikitMenu, ConnectorNames } from '@pancakeswap-libs/uikit'
+import { useWeb3React } from '@web3-react/core'
+import { allLanguages } from 'constants/localisation/languageCodes'
+import { LanguageContext } from 'hooks/LanguageContext'
+import useTheme from 'hooks/useTheme'
+import useGetPriceData from 'hooks/useGetPriceData'
+import useGetLocalProfile from 'hooks/useGetLocalProfile'
+import { connectorsByName } from 'connectors'
+import links from './config'
+
+const Menu: React.FC = (props) => {
+  const { account, activate, deactivate } = useWeb3React()
+  const { selectedLanguage, setSelectedLanguage } = useContext(LanguageContext)
+  const { isDark, toggleTheme } = useTheme()
+  const priceData = useGetPriceData()
+  const cakePriceUsd = priceData ? Number(priceData.prices.Cake) : undefined
+  const profile = useGetLocalProfile()
+
+  return (
+    <UikitMenu
+      links={links}
+      account={account as string}
+      login={(connectorId: ConnectorNames) => {
+        const connector = connectorsByName[connectorId]
+        if (connector) {
+          activate(connector)
+        }
+      }}
+      logout={deactivate}
+      isDark={isDark}
+      toggleTheme={toggleTheme}
+      currentLang={selectedLanguage?.code || ''}
+      langs={allLanguages}
+      setLang={setSelectedLanguage}
+      // cakePriceUsd={cakePriceUsd}
+      // profile={profile}
+      {...props}
+    />
+  )
+}
+
+export default Menu
