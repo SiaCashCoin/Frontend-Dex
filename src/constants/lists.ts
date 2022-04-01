814d9865518d92b0dcd25b2b62a6798f5cacb462
SiaCashCoin <39414933+SiaCashCoin@users.noreply.github.com>
2022-04-01 15:02:37 -0400

    Update lists.ts

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -2,4 +2,4 @@ export const DEFAULT_TOKEN_LIST_URL = 'pancakeswap'
 const CMC = 'https://tokens.pancakeswap.finance/cmc.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [CMC, DEFAULT_TOKEN_LIST_URL]
-
+export const OFFICIAL_LISTS = [CMC]
