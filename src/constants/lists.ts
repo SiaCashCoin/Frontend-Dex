595eb68605bbf4bdf2f5b0eb46b737bb57531948
SiaCashCoin <39414933+SiaCashCoin@users.noreply.github.com>
2022-04-01 21:14:41 -0400

    Update lists.ts

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,5 +1,4 @@
 export const DEFAULT_TOKEN_LIST_URL = 'CoinGecko'
-
 export const SCC_TOKEN_LIST_URL = 'pancakeswap'
 
-export const DEFAULT_LIST_OF_LISTS: string[] = [DEFAULT_TOKEN_LIST_URL, SCC_TOKEN_LIST_URL,]
+export const DEFAULT_LIST_OF_LISTS: string[] = [DEFAULT_TOKEN_LIST_URL, SCC_TOKEN_LIST_URL]
